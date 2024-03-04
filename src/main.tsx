32981733f4c4a94112b6f1c1977446d9c5512c88
fatelexx <fatelexx@outlook.com>
2024-03-04 22:05:06 +0800

    chore: tailwind working

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import App from './app/app';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <h1 className="text-3xl font-bold underline text-blue-400">
+      Hello world from Floren Alexis!
+    </h1>
   </StrictMode>
 );
