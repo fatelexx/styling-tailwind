08ec63fff8a891142d254303b0e1923015a65046
fatelexx <fatelexx@outlook.com>
2024-03-21 14:21:31 +0800

    chore: add playground sample

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -103,36 +103,103 @@ export default function Login() {
                 </div>
             </div>
         </section>
-        : <Dashboard />
+        : <Playground />
     )
 }
 
-export function Dashboard() {
+export function Playground() {
     return (
-        <div className="flex h-full flex-col px-3 py-4 md:px-2">
-          <a
-            className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-            href="/"
-          >
-            <div className="w-32 text-white md:w-40">
-              {/* <AcmeLogo /> */}
-            </div>
-          </a>
-          <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-            {/* <NavLinks /> */}
-            <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-            <form
-                    //   action={async () => {
-                    //     'use server';
-                    //     await signOut();
-                    //   }}
+        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <img
+          src="/img/beams.jpg"
+          alt=""
+          className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+          width={1308}
+        />
+        <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
+        <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+          <div className="mx-auto max-w-md">
+            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" className="h-6" alt="Tailwind Play" />
+            <div className="divide-y divide-gray-300/50">
+              <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
+                <p>
+                  An advanced online playground for Tailwind CSS, including support
+                  for things like:
+                </p>
+                <ul className="space-y-4">
+                  <li className="flex items-center">
+                    <svg
+                      className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
-              <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-                {/* <PowerIcon className="w-6" /> */}
-                <div className="hidden md:block">Sign Out</div>
-              </button>
-            </form>
+                      <circle cx={12} cy={12} r={11} />
+                      <path
+                        d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                        fill="none"
+                      />
+                    </svg>
+                    <p className="ml-4">
+                      Customizing your
+                      <code className="text-sm font-bold text-gray-900">
+                        tailwind.config.js
+                      </code>{" "}
+                      file
+                    </p>
+                  </li>
+                  <li className="flex items-center">
+                    <svg
+                      className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <circle cx={12} cy={12} r={11} />
+                      <path
+                        d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                        fill="none"
+                      />
+                    </svg>
+                    <p className="ml-4">
+                      Extracting classes with
+                      <code className="text-sm font-bold text-gray-900">
+                        @apply
+                      </code>
+                    </p>
+                  </li>
+                  <li className="flex items-center">
+                    <svg
+                      className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <circle cx={12} cy={12} r={11} />
+                      <path
+                        d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                        fill="none"
+                      />
+                    </svg>
+                    <p className="ml-4">Code completion with instant preview</p>
+                  </li>
+                </ul>
+                <p>
+                  Perfect for learning how the framework works, prototyping a new
+                  idea, or creating a demo to share online.
+                </p>
+              </div>
+              <div className="pt-8 text-base font-semibold leading-7">
+                <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
+                <p>
+                  <a
+                    href="https://tailwindcss.com/docs"
+                    className="text-sky-500 hover:text-sky-600"
+                  >
+                    Read the docs →
+                  </a>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
       );
 }
